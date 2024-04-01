6fc6a54ff43b04fd887cdd7ef66d042405352459
Danilo <danilogmoura@gmail.com>
2024-04-01 07:01:07 -0300

    Clain todas as quests gerais

---
+++ Hooks/QUEST.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+
+namespace IEHHook.Hooks
+{
+    // using System;
+    // using System.Collections.Generic;
+    // using System.Runtime.CompilerServices;
+    //
+    // // Token: 0x0200048B RID: 1163
+    // public partial class QUEST
+    // {
+    //     // Token: 0x06002674 RID: 9844 RVA: 0x000D30C0 File Offset: 0x000D12C0
+    //     public bool CanClaim()
+    //     {
+    //         if (!this.isAccepted)
+    //         {
+    //             return false;
+    //         }
+    //         if (this.isCleared)
+    //         {
+    //             return false;
+    //         }
+    //         for (int i = 0; i < this.clearConditions.Count; i++)
+    //         {
+    //             if (!this.clearConditions[i]())
+    //             {
+    //                 return false;
+    //             }
+    //         }
+    //         if (this.type == QuestType.Bring)
+    //         {
+    //             foreach (KeyValuePair<MaterialKind, double> keyValuePair in this.bringRequiredMaterials)
+    //             {
+    //                 if (this.TargetMaterial(keyValuePair.Key).value < keyValuePair.Value)
+    //                 {
+    //                     return false;
+    //                 }
+    //             }
+    //             foreach (KeyValuePair<ResourceKind, double> keyValuePair2 in this.bringRequiredResources)
+    //             {
+    //                 if (this.TargetResource(keyValuePair2.Key).value < keyValuePair2.Value)
+    //                 {
+    //                     return false;
+    //                 }
+    //             }
+    //         }
+    //         return (this.type != QuestType.Defeat || this.DefeatTargetMonsterDefeatedNum() >= this.defeatRequredDefeatNum()) && (this.type != QuestType.Capture || this.CaptureTargetMonsterCapturedNum() >= this.captureRequiredNum()) && (this.type != QuestType.AreaComplete || this.AreaCompletedNum() >= this.areaRequredCompletedNum());
+    //     }
+    // }
+    [HarmonyPatch(typeof(QUEST), "CanClaim")]
+    public static class QUEST_CanClaim
+    {
+        public static void Postfix(QUEST __instance, ref bool __result)
+        {
+            if (__instance.kind == QuestKind.General)
+            {
+                __result = true;
+            }
+        }
+    }
+}
---
+++ Hooks/QuestController.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+
+namespace IEHHook.Hooks
+{
+    // using System;
+    // using System.Collections.Generic;
+    // using System.Runtime.CompilerServices;
+    //
+    // // Token: 0x02000496 RID: 1174
+    // public partial class QuestController
+    // {
+    //     // Token: 0x060026DC RID: 9948 RVA: 0x000D8224 File Offset: 0x000D6424
+    //     public void ClaimFavorite(HeroKind heroKind)
+    //     {
+    //         if (GameController.game.battleCtrls[(int)heroKind].isActiveBattle)
+    //         {
+    //             for (int i = 0; i < this.generalQuests[(int)heroKind].Length; i++)
+    //             {
+    //                 if (this.generalQuests[(int)heroKind][i].isFavorite)
+    //                 {
+    //                     this.generalQuests[(int)heroKind][i].Claim();
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    [HarmonyPatch(typeof(QuestController), "ClaimFavorite", typeof(HeroKind))]
+    public static class QuestController_ClaimFavorite
+    {
+        public static void Postfix(QuestController __instance, HeroKind heroKind)
+        {
+            var quests = Traverse.Create(__instance).Field("generalQuests").GetValue<QUEST[][]>();
+            if (GameController.game.battleCtrls[(int)heroKind].isActiveBattle)
+                for (var i = 0; i < quests[(int)heroKind].Length; i++)
+                {
+                    quests[(int)heroKind][i].isFavorite = true;
+                    quests[(int)heroKind][i].Claim();
+                }
+        }
+    }
+}
