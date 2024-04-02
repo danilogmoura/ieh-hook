3a940fe1649a302e26b221b0fb0efbe0534c2922
Danilo <danilogmoura@gmail.com>
2024-04-02 14:35:20 -0300

    Velocidade da pesquisa de 0.01 para 1_000_000

---
+++ Hooks/TownController.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+
+namespace IEHHook.Hooks
+{
+    // using System;
+    //
+    // // Token: 0x02000599 RID: 1433
+    // public partial class TownController
+    // {
+    //     // Token: 0x06002D60 RID: 11616 RVA: 0x00119AC4 File Offset: 0x00117CC4
+    //     public void ProgressResaerch(double deltaTime)
+    //     {
+    //         for (int i = 0; i < this.buildings.Length; i++)
+    //         {
+    //             this.buildings[i].Update(deltaTime);
+    //         }
+    //     }
+    // }
+
+    [HarmonyPatch(typeof(TownController), "ProgressResaerch", typeof(double))]
+    public class TownController_ProgressResaerch
+    {
+        public static bool Prefix(TownController __instance, ref double deltaTime)
+        {
+            deltaTime = 1_000_000;
+            return true;
+        }
+    }
+}
