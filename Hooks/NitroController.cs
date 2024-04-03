19e800843ae964f735ce94d16455d9612c52b90c
Danilo <danilogmoura@gmail.com>
2024-04-02 22:49:55 -0300

    Ajuste da velocidade máxima usando o nitro

--- Hooks/NitroController.cs
+++ Hooks/NitroController.cs
@@ -7,45 +7,29 @@ namespace IEHHook.Hooks
     // // Token: 0x020005BD RID: 1469
     // public partial class NitroController
     // {
-    //     // Token: 0x06002E83 RID: 11907 RVA: 0x0011CFC4 File Offset: 0x0011B1C4
-    //     public void SwitchActive(bool isActive)
+    //     // Token: 0x06002E81 RID: 11905 RVA: 0x0011CE98 File Offset: 0x0011B098
+    //     public NitroController()
     //     {
-    //         if (isActive && this.nitro.value < 1.0)
-    //         {
-    //             isActive = false;
-    //         }
-    //         this.isActive = isActive;
-    //         if (isActive)
-    //         {
-    //             this.nitro.Decrease(this.nitroConsumption, false);
-    //             this.lastTime = Main.main.allTimeRealtime;
-    //             this.maxNitroSpeed.Calculate(false);
-    //             this.speed.CheckMaxAndMin();
-    //             Main.main.ChangeTimeScale((float)this.speed.value);
-    //         }
-    //         else
-    //         {
-    //             Main.main.ChangeTimeScale(1f);
-    //         }
-    //         if (this.switchUIAction != null)
-    //         {
-    //             this.switchUIAction();
-    //         }
+    //         this.nitroTimescale = 2f;
+    //         base..ctor();
+    //         this.nitroCap = new Multiplier(new MultiplierInfo(MultiplierKind.Base, MultiplierType.Add, () => 5000.0, null), null, null);
+    //         this.nitroCap.Calculate(false);
+    //         this.nitro = new Nitro(() => this.nitroCap.Value());
+    //         this.nitro.runOutAction = new Action(this.AutoAlchemiseForNitroAction);
+    //         this.maxNitroSpeed = new Multiplier(new MultiplierInfo(MultiplierKind.Base, MultiplierType.Add, () => 2.0, null), null, null);
+    //         this.speed = new NitroSpeed(() => this.maxNitroSpeed.Value(), () => 1.5);
+    //         this.maxNitroSpeed.RegisterMultiplier(new MultiplierInfo(MultiplierKind.Base, MultiplierType.Add, () => (double)(this.nitroTimescale - 2f), null));
     //     }
     // }
 
-    [HarmonyPatch(typeof(NitroController), "SwitchActive", typeof(bool))]
-    public static class NitroController_SwitchActive
+    [HarmonyPatch(typeof(NitroController), MethodType.Constructor)]
+    public static class NitroController_Constructor
     {
-        public static bool Prefix(NitroController __instance, bool isActive)
+        public static void Postfix(NitroController __instance)
         {
-            __instance.maxNitroSpeed = new Multiplier(new MultiplierInfo(MultiplierKind.Base, MultiplierType.Add,
-                () => 20.0));
-            __instance.speed = new NitroSpeed(() => __instance.maxNitroSpeed.Value(), () => 15.0);
-            __instance.maxNitroSpeed.RegisterMultiplier(new MultiplierInfo(MultiplierKind.Base, MultiplierType.Add,
-                () => __instance.nitroTimescale - 2f));
-
-            return true;
+            __instance.maxNitroSpeed =
+                new Multiplier(new MultiplierInfo(MultiplierKind.Base, MultiplierType.Add, () => 20.0));
+            __instance.speed = new NitroSpeed(() => __instance.maxNitroSpeed.Value(), () => 20.0);
         }
     }
 }
