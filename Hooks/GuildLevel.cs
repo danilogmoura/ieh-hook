30052cb846fbc31166607901a1ef714e19fa0561
Danilo <danilogmoura@gmail.com>
2024-04-04 21:16:23 -0300

    Upar rapidamente a cidade

---
+++ Hooks/GuildLevel.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+
+namespace IEHHook.Hooks
+{
+    // using System;
+    //
+    // // Token: 0x020003A9 RID: 937
+    // public partial class GuildLevel : INTEGER
+    // {
+    //     // Token: 0x0600228D RID: 8845 RVA: 0x000C3160 File Offset: 0x000C1360
+    //     public override void Increase(long increment)
+    //     {
+    //         base.Increase(increment);
+    //         GameController.game.guildCtrl.abilityPointLeft.Increase((double)increment, false);
+    //         int num = 0;
+    //         while ((long)num < increment)
+    //         {
+    //             checked
+    //             {
+    //                 GameController.game.guildCtrl.accomplishGuildLevels[(int)((IntPtr)Math.Max(0L, unchecked(this.value - (long)num)))].RegisterTime();
+    //             }
+    //             num++;
+    //         }
+    //         if (this.logUIAction != null)
+    //         {
+    //             this.logUIAction();
+    //         }
+    //         Main.main.S.maxGuildLevel = Math.Max(Main.main.S.maxGuildLevel, this.value);
+    //     }
+    // }
+
+    [HarmonyPatch(typeof(GuildLevel), "Increase")]
+    public static class GuildLevelIncreasePatch
+    {
+        public static void Prefix(GuildLevel __instance, ref long increment)
+        {
+            increment *= 2;
+        }
+    }
+}
