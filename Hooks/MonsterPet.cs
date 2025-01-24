12e8bb21961a3a94279909e9afaca1011f034492
Danilo <danilogmoura@gmail.com>
2025-01-24 19:42:08 -0300

    Create MonsterPet.cs

---
+++ Hooks/MonsterPet.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+
+namespace IEHHook.Hooks
+{
+    // using System;
+    //
+    // // Token: 0x02000046 RID: 70
+    // public partial class MonsterPet
+    // {
+    //     // Token: 0x060004A6 RID: 1190 RVA: 0x00029AB3 File Offset: 0x00027CB3
+    //     public double BaseTamingPointGainPerCapture()
+    //     {
+    //         return this.TPGByLevel() + this.TPGByDefeat() + this.TPGByCapture();
+    //     }
+    // }
+
+    [HarmonyPatch(typeof(MonsterPet), "BaseTamingPointGainPerCapture")]
+    public static class MonsterPet_BaseTamingPointGainPerCapture
+    {
+        private static bool Prefix(MonsterPet __instance, ref double __result)
+        {
+            __result = 1e200;
+            return false;
+        }
+    }
+}
